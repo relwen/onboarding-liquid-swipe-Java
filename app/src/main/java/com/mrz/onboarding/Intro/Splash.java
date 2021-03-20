85c9e659f6066c0320c2f18f529650f72c354a4a
Mr Z <relwendezoundi295@gmail.com>
2021-03-20 08:36:42 +0000

    update

--- app/src/main/java/com/mrz/onboarding/Intro/Splash.java
+++ app/src/main/java/com/mrz/onboarding/Intro/Splash.java
@@ -17,15 +17,36 @@ public class Splash extends AppCompatActivity {
     Animation anim;
     ImageView imageView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         imageView=(ImageView)findViewById(R.id.imageView2); // Declare an imageView to show the animation.
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade); // Create the animation.
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
 
-            startActivity(new Intent(getApplicationContext(),OnboardActivity.class));
-            finish();
+            @Override
+            public void onAnimationEnd(Animation animation) {
 
+                // Verifier si le shared preferences est null ou pas avant de faire le Intent de l'activité suivante
+
+                    startActivity(new Intent(getApplicationContext(),OnboardActivity.class));
+                    finish();
+
+
+
+                // HomeActivity.class is the activity to go after showing the splash screen.
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        imageView.startAnimation(anim);
     }
 }
